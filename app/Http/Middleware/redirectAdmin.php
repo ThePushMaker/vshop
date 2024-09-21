8310cdaf0da353c53c500d39837437394093c41b
Martín Calderón <elmcwl@gmail.com>
2024-09-21 02:15:01 -0700

    feat: add redirectAdmin middleware

--- app/Http/Middleware/redirectAdmin.php
+++ app/Http/Middleware/redirectAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,8 +14,12 @@ class redirectAdmin
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $guard = null): Response
     {
+        if (Auth::guard($guard)->check() && auth()->user()->isAdmin == 1) {
+            return redirect()->route('admin.dashboard');
+        }
+        
         return $next($request);
     }
 }
