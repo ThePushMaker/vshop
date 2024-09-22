a34c8273336615926889aa1f3c4df56accf816f4
Martín Calderón <elmcwl@gmail.com>
2024-09-21 23:04:42 -0700

    feat: add flowbite dashboard ui component

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminController extends Controller
 {
     public function index() {
-        return 'ok';
+        
+        return Inertia::render('Admin/Dashboard');
     }
 }
