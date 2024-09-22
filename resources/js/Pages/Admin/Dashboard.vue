641b815c82b654cb94c1d2a03b673d56ad4d0593
Martín Calderón <elmcwl@gmail.com>
2024-09-22 01:46:20 -0700

    feat: add javascript to flowbite dashboard component

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -809,5 +809,11 @@
 </template>
 
 <script setup>
+  import { onMounted } from 'vue'
+  import { initFlowbite } from 'flowbite'
 
+  // initialize components based on data attribute selectors
+  onMounted(() => {
+      initFlowbite();
+  })
 </script>
