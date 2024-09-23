87c5970001362559cd658ceb969bc2de3ee66704
Martín Calderón <elmcwl@gmail.com>
2024-09-23 06:16:25 -0700

    add components for Admin/Dashboard.vue

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    
+    <!-- Navbar start -->
+    <Navbar/>
+    <!-- end -->
+
+    <!-- Sidebar star -->
+    <Sidebar/>
+    <!-- end -->
+
+    <slot/>
+  </div>
+</template>
+
+<script setup>
+  import { onMounted } from 'vue'
+  import { initFlowbite } from 'flowbite'
+  import Navbar from './Navbar.vue';
+  import Sidebar from './Sidebar.vue';
+
+  // initialize components based on data attribute selectors
+  onMounted(() => {
+      initFlowbite();
+  })
+</script>
