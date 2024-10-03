8791760d5ba2ae4a31b63001c23d60a6dbbd4f07
Martín Calderón <elmcwl@gmail.com>
2024-10-03 04:48:13 -0700

    feat: add orders migration

--- database/migrations/2024_09_27_040040_create_orders_table.php
+++ database/migrations/2024_09_27_040040_create_orders_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\UserAddress;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_price',20, 2);
+            $table->string('status', 45);
+            $table->string('session_id', 255);
+            $table->foreignIdFor(UserAddress::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
             $table->timestamps();
         });
     }
